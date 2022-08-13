8c18ce5e7773b7d5afcdbb758e4ae222b97d1b56
gonzalocastellan0 <gonzalo.castellano1998@gmail.com>
2022-08-13 17:59:27 -0300

    1er commit - Se agrega el proyecto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let header__navbar = document.querySelector('#header__navbar');
+
+let header__menu = document.querySelector('#header__menu');
+
+    header__menu.addEventListener('click', function(){
+    header__navbar.classList.toggle('header__navabar--toggle');
+    })
